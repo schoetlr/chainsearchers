70f0aa19e83e2030a9e784ec93f4efa73afa38dc
schoetlr <srlrs20020@gmail.com>
2016-11-07 16:28:24 -0500

    get index state for lists to load

--- app/assets/javascripts/controllers/lists_ctrl.js
+++ app/assets/javascripts/controllers/lists_ctrl.js
@@ -1,5 +1,7 @@
 app.controller("ListsCtrl", ['$scope', 'listService', function($scope, listService){
 
+  console.log("loaded proper controller");
+
   listService.getPopularLists().then(function(response){
     $scope.lists = response;
   }, function(){
