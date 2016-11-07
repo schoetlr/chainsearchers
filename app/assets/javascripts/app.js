34397c147ca8d3fe72d1d9aab6209f204a9d5e22
schoetlr <srlrs20020@gmail.com>
2016-11-07 08:53:29 -0500

    set up angular app and folders

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,58 @@
+var app = angular.module('app', ['ui.router', 'restangular', 'angularModalService']);
+
+
+
+app.config([
+  "$httpProvider",
+  function($httpProvider) {
+    var token = $('meta[name=csrf-token]').attr('content');
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = token;
+  }
+]);
+
+app.config([
+  'RestangularProvider',
+  function(RestangularProvider) {
+
+    
+    RestangularProvider.setRequestSuffix('.json');
+    RestangularProvider.setDefaultHttpFields({"content-type": "application/json"});
+  }
+]);
+
+
+
+app.config(function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+    .state("lists", {
+      url: "/",
+      abstract: true,
+      template: '<div ui-view></div>'
+    })
+
+    .state("lists.index", {
+      url: "",
+      templateUrl: '/templates/lists/index.html',
+      controller: "ListsCtrl"
+    })
+
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+app.factory("_", ['$window', function($window){
+  return $window._;
+}]);
