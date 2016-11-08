923640b65bd03ee50ac62d52015b663e4c03077e
schoetlr <srlrs20020@gmail.com>
2016-11-08 11:11:41 -0500

    loading link

--- app/assets/javascripts/controllers/browse_ctrl.js
+++ app/assets/javascripts/controllers/browse_ctrl.js
@@ -1,5 +1,36 @@
-app.controller("BrowseCtrl", ['$scope', 'list', 'links', function($scope, list, links){
+app.controller("BrowseCtrl", ['$scope', 'list', 'links', 'selectedIndex', '$sce', function($scope, list, links, selectedIndex, $sce){
 
-  
+  $scope.list = list;
+
+  $scope.links = links;
+
+  $scope.selectedIndex = selectedIndex;
+  $scope.selectedLink = $scope.links[$scope.selectedIndex];
+
+  $scope.generatePath = function(){
+    if($scope.selectedLink.url[0] === 'w'){
+      var url = 'http://' + $scope.selectedLink.url;
+      return $sce.trustAsResourceUrl(url);
+    } else {
+      return $sce.trustAsResourceUrl($scope.selectedLink.url);
+    }
+  };
+
+  $scope.next = function(){
+    if($scope.selectedIndex < ($scope.links.length - 1)){
+      $scope.selectedIndex += 1;
+
+      $scope.selectedLink = $scope.links[$scope.selectedIndex];
+    }
+
+  };
+
+  $scope.previous = function(){
+    if($scope.selectedIndex > 0){
+      $scope.selectedIndex -= 1;
+
+      $scope.selectedLink = $scope.links[$scope.selectedIndex];
+    }
+  };
 
 }]);
--- app/assets/javascripts/controllers/lists_ctrl.js
+++ app/assets/javascripts/controllers/lists_ctrl.js
@@ -23,13 +23,16 @@ app.controller("ListsCtrl", ['$scope', 'listService', 'ModalService', function($
 
   $scope.browseList = function(list){
     var links = $scope.listLinks(list);
+    
+    var selectedIndex = 0;
 
     ModalService.showModal({
       templateUrl: "/templates/lists/browse.html",
       controller: "BrowseCtrl",
       inputs: {
         list: list,
-        links: links
+        links: links, 
+        selectedIndex: selectedIndex
       }
     }).then(function(modal) {
     
