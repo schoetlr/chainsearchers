a1ad41c722380182e5334c8deae5a1f956e03b7f
schoetlr <srlrs20020@gmail.com>
2016-11-16 14:26:19 -0500

    comment service and controller good to go

--- app/assets/javascripts/services/comment_service.js
+++ app/assets/javascripts/services/comment_service.js
@@ -1,7 +1,19 @@
 app.factory("commentService", ['Restangular', function(Restangular){
   var service = {};
 
+  service.commentOnList = function(formData){
+    var data = { commentable_type: "List", commentable_id: formData.commentableId, content: formData.content };
 
+    var params = { comment: data };
+    return Restangular.all("comments").post(params);
+  };
+
+  service.respondToComment = function(){
+    var data = { commentable_type: "Comment", commentable_id: formData.commentableId, content: formData.content };
+
+    var params = { comment: data };
+    return Restangular.all("comments").post(params);
+  };
 
   return service;
 }]);
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    
+    if @comment.save
+      respond_to do |format|
+        format.json { render json: @comment }
+      end
+    else
+      respond_to do |format|
+        format.json {}
+      end
+    end
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:commentable_type, :commentable_id, :content)
+  end
 end
