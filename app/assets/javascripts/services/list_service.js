bf97870ac68e199eda5c2fd7969901ab5ff59717
schoetlr <srlrs20020@gmail.com>
2017-08-08 18:34:38 -0400

    remove redundant methods

--- app/assets/javascripts/services/list_service.js
+++ app/assets/javascripts/services/list_service.js
@@ -8,26 +8,8 @@ app.factory("listService", ['Restangular', function(Restangular){
     return Restangular.all("lists").getList(params);
   };
 
-  service.getPopularLists = function(){
-    return Restangular.all("lists").getList();
-  };
-
-  service.getRecentLists = function(){
-    var params = {option: "Recent"}
-    return Restangular.all("lists").getList(params);
-  };
-
   service.createList = function(listData){
     return Restangular.all("lists").post(listData);
-  };
-
-  service.getListsByTag = function(tags){
-    var params = { "tags[]": tags };
-    
-    return Restangular.all("lists").getList(params);
-    //get list is turning the params into an object
-
-    //could store previous tags in session
   };
 
   service.getList = function(id){
