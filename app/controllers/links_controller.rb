b8b5664b9e03cee1935ace768fcdbcd7fd99069b
schoetlr <srlrs20020@gmail.com>
2017-10-30 13:04:37 -0400

    remove unused methods in links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,55 +1,8 @@
-#THE ONLY THING BEING USED IN THIS CONTROLLER IS DESTROY.
-#I just haven't got around to cleaning it up.
-#create and update logic was moved to the model so it can be called from the lists controller
-
 
 class LinksController < ApplicationController
-  before_action :validate_ownership, only: [:update, :destroy]
+  before_action :validate_ownership, only: [:destroy]
 
-  def create
-    list_id = params[:list_id].to_i
-    list_links = []
-    @list = List.find(list_id)
-
-    params[:links] = !params[:links] ? [] : params[:links]
-
-    params[:links].each do |link_data|
-      next if !link_data[:url] && !link_data[:description]
-
-      link = Link.new(link_params(link_data))
-      link.list_id = list_id
-      link.save
-      #give the newly created link the proper parent
-      if list_links.length > 0
-        link.link_id = list_links.last.id
-        link.save
-      #don't think this is being used...
-      elsif params[:update] #if creating from EditListCtrl do this
-        last_link = @list.last_link
-        link.link_id = last_link.id
-        link.save
-      end
-
-      list_links.push(link)
-    end
-
-    respond_to do |format|
-      format.json { render json: {} }
-    end
-
-  end
-
-  def update
-    @link = Link.find(params[:id])
-    @link.url = params[:url]
-    @link.description = params[:description]
-
-    if @link.save
-      respond_to do |format|
-        format.json { render json: @link }
-      end
-    end    
-  end
+  
 
   def destroy
     @link = Link.find(params[:id])
@@ -74,7 +27,5 @@ class LinksController < ApplicationController
     end
   end
 
-  def link_params(link_data)
-    link_data.permit(:url, :description)
-  end
+  
 end
