114f1240fca4aee5064ec0e36a8ac6d7d2ab1431
schoetlr <srlrs20020@gmail.com>
2017-11-13 20:44:14 -0500

    change production smtp port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,10 +78,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {:host => 'chainsearchers.com'}
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "127.0.0.1",
-    :port    => 25,
+    :port    => 587,
     :domain  => 'chainsearchers.com'
   }
 end
