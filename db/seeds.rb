60b989efe90323c5f38e30acb21cecf363fbfb3a
schoetlr <srlrs20020@gmail.com>
2017-11-14 15:39:53 -0500

    fix seed bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ category_names.each do |name|
 end
 
 
-user = User.create(username: "test_user", email: testemail@email.com, password: "password", password_confirmation: "password", confirmed_at: Time.now)
+user = User.create(username: "test_user", email: "testemail@email.com", password: "password", password_confirmation: "password", confirmed_at: Time.now)
 
 description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum."
 
