940ee6f1513f4134f5491911b47408a9f7691577
schoetlr <srlrs20020@gmail.com>
2017-11-15 12:41:47 -0500

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@ category_names.each do |name|
 end
 
 
-user = User.create(username: "test_user", email: "testemail@email.com", password: "password", password_confirmation: "password", confirmed_at: Time.now)
-
+user = User.create(username: "test_user_xyz", email: "testemail@email.com", password: "password", password_confirmation: "password", confirmed_at: Time.now)
+byebug
 UserWall.create(user_id: user.id)
 
 description = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum."
