88e0368b57e72a1f8e8aeeeffbf351b569bcd8b5
schoetlr <srlrs20020@gmail.com>
2017-11-16 10:31:47 -0500

    log uri info

--- app/controllers/doorkeeper_authorizations_controller.rb
+++ app/controllers/doorkeeper_authorizations_controller.rb
@@ -8,10 +8,11 @@ class Doorkeeper::AuthorizationsController
   def correct_uri
     if params[:redirect_uri]
       uri = params[:redirect_uri]
-
+      logger.debug "The original URI in params is: " + uri
       if uri[0..6] == "https:/" && uri[7] != "/"
-        params[:redirect_uri] = "https://"
+        params[:redirect_uri] = "https://" + params[:redirect_uri][7..-1]
       end
+      logger.debug "The altered URI is: " + params[:redirect_uri]
     end
   end
 
