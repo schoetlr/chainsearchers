dd779daabb1ab5c3d8f5bbfe4f88055ff95b1ccb
schoetlr <srlrs20020@gmail.com>
2017-11-17 21:14:41 -0500

    small change to config

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Chainsearchers
       DeviseController.respond_to :html, :json
     end
 
-    config.middleware.insert_before ActionDispatch::Static, "Rack::Cors" do
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
