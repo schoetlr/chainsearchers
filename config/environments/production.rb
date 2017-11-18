8e2d79f17bda7bb155e5049c66a35898c1e2cf51
schoetlr <srlrs20020@gmail.com>
2017-11-18 18:13:12 -0500

    remove serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  #add this to try to fix CORS block
-  config.serve_static_assets = true
+  # #add this to try to fix CORS block
+  # config.serve_static_assets = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
