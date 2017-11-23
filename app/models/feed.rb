51cc70443540cc6f5020ad41bfb87f9720f1200b
schoetlr <srlrs20020@gmail.com>
2017-11-23 08:17:38 -0500

    order feed by desc time

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,7 +3,7 @@ class Feed
   def self.personal_feed(current_user)
     followed_ids = current_user.followed_users.pluck(:id).to_a
     
-    List.where('user_id in (?)', followed_ids)
+    List.where('user_id in (?)', followed_ids).order(created_at: :desc)
     
   end
 
